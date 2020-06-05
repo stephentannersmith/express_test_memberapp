8192f7b691d91ddec997e5b22f38e6fea134e215
Tanner Smith <tanner.ssu@gmail.com>
2020-06-05 15:47:27 -0500

    init git

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1, 
+        name: 'John Doe',
+        email: 'jd@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2, 
+        name: 'Jan Doe',
+        email: 'j@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3, 
+        name: 'Tanner Smith',
+        email: 'tanner@gmail.com',
+        status: 'inactive'
+    }
+];
+
+module.exports = members;
